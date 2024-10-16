607256bdebba1fe0b4f8e04a44cad51e8e281e3f
HiranRr <hiranribasribeiro@gmail.com>
2024-10-16 20:33:35 -0300

    790707070

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+let botao = document.getElementById('btn')
+
+function tentei(){
+    document.getElementById('btn').innerHTML = "comprado"
+    document.getElementById('btn').style.borderColor = "white"
+    document.getElementById('btn').style.transition = "2s"
+    document.body.style.backgroundColor = "#333333"
+    document.body.style.transition = "2s"
+}
